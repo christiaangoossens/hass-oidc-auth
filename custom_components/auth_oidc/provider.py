dd295942911ec4214963ed9d5787e988c2c61a19
Christiaan Goossens <contact@christiaangoossens.nl>
2024-12-31 16:48:28 +0100

    Slow down code checking to 200ms

--- custom_components/auth_oidc/provider.py
+++ custom_components/auth_oidc/provider.py
@@ -3,6 +3,7 @@ Allow access to users based on login with an external OpenID Connect Identity Pr
 """
 
 import logging
+import bcrypt
 
 from typing import Dict, Optional
 import asyncio
@@ -274,6 +275,14 @@ class OpenIdLoginFlow(LoginFlow):
     """Handler for the login flow."""
 
     async def _finalize_user(self, code: str) -> AuthFlowResult:
+        # Verify a dummy hash to make it last a bit longer
+        # as security measure (limits the amount of attempts you have in 5 min)
+        # Similar to what the HomeAssistant auth provider does
+        dummy = b"$2b$12$CiuFGszHx9eNHxPuQcwBWez4CwDTOcLTX5CbOpV6gef2nYuXkY7BO"
+        bcrypt.checkpw(b"foo", dummy)
+
+        # Actually look up the auth provider after,
+        # this doesn't take a lot of time (regardless of it's in there or not)
         sub = await self._auth_provider.async_get_subject(code)
         if sub:
             return await self.async_finish(
