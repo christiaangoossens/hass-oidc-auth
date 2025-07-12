bcefa455518ca66a2084f0d3c20c7ed0bff835cb
Joshua Tag Howard <tag@jthoward.dev>
2025-07-12 13:39:56 -0400

    Tweak code field error status

--- custom_components/auth_oidc/static/injection.js
+++ custom_components/auth_oidc/static/injection.js
@@ -1,27 +1,42 @@
 function safeSetTextContent(element, value) {
   if (!element) return
   var textNode = Array.from(element.childNodes).find(node => node.nodeType === Node.TEXT_NODE && node.textContent.trim().length > 0)
-  if (!textNode) return
+  if (!textNode || textNode.textContent === value) return
   textNode.textContent = value
 }
 
+let firstFocus = true
+
 function addSSOButton() {
   const loginHeader = document.querySelector(".card-content > ha-auth-flow > form > h1")
-  safeSetTextContent(loginHeader, "Log in to Home Assistant")
-  
   const codeField = document.querySelector(".mdc-text-field__input[name=code]")
   const loginButton = document.querySelector("mwc-button:not(.sso)")
+  const ssoButton = document.querySelector("mwc-button.sso")
 
-  if (codeField) {
+  safeSetTextContent(loginHeader, "Log in to Home Assistant")
+
+  if (codeField && codeField.placeholder !== "One-time code") {
     codeField.placeholder = "One-time code"
     codeField.autofocus = false
     codeField.autocomplete = "off"
-    setTimeout(() => {
-      codeField.blur()
-    }, 0)
+    if (firstFocus) {
+      firstFocus = false
+      if (document.activeElement === codeField) {
+        setTimeout(() => {
+          codeField.blur()
+          setTimeout(() => {
+            const helperText = document.querySelector("#helper-text")
+            const invalidTextField = document.querySelector(".mdc-text-field--invalid")
+            const validationMsg = document.querySelector(".mdc-text-field-helper-text--validation-msg")
+            if (helperText) safeSetTextContent(helperText, "")
+            if (invalidTextField) invalidTextField.classList.remove("mdc-text-field--invalid")
+            if (validationMsg) validationMsg.classList.remove("mdc-text-field-helper-text--validation-msg")
+          }, 0)
+        }, 0)
+      }
+    }
   }
 
-  var ssoButton = document.querySelector("mwc-button.sso")
   if (!ssoButton) {
     ssoButton = document.createElement("mwc-button")
     ssoButton.classList.add("sso")
@@ -32,14 +47,16 @@ function addSSOButton() {
     ssoButton.addEventListener("click", () => {
       location.href = "/auth/oidc/redirect"
     })
-    loginButton.parentElement.prepend(ssoButton)
+    if (loginButton) loginButton.parentElement.prepend(ssoButton)
+  } else {
+    ssoButton.style.display = codeField ? "" : "none"
   }
 
   safeSetTextContent(loginButton, codeField ? "Log in with code" : "Log in")
-  ssoButton.style.display = codeField ? "" : "none"
 }
 
 const observer = new MutationObserver((mutationsList, observer) => {
   addSSOButton()
 })
+
 observer.observe(document.body, { childList: true, subtree: true })
