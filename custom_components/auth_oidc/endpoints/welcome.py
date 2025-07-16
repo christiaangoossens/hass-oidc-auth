75810aab06f719454f6988c64997534f887f146b
Christiaan Goossens <contact@christiaangoossens.nl>
2025-07-16 12:16:22 +0200

    Force HTTPS feature

--- custom_components/auth_oidc/endpoints/welcome.py
+++ custom_components/auth_oidc/endpoints/welcome.py
@@ -14,15 +14,16 @@ class OIDCWelcomeView(HomeAssistantView):
     url = PATH
     name = "auth:oidc:welcome"
 
-    def __init__(self, name: str, is_frontend_injection_enabled: bool) -> None:
+    def __init__(self, name: str, is_enabled: bool, force_https: bool) -> None:
         self.name = name
-        self.is_enabled = not is_frontend_injection_enabled
+        self.is_enabled = is_enabled
+        self.force_https = force_https
 
     async def get(self, _: web.Request) -> web.Response:
         """Receive response."""
 
         if not self.is_enabled:
-            return web.HTTPTemporaryRedirect(get_url("/"))
+            return web.HTTPTemporaryRedirect(get_url("/", self.force_https))
 
         view_html = await get_view("welcome", {"name": self.name})
         return web.Response(text=view_html, content_type="text/html")
