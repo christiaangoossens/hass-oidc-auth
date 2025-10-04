5dbef208c20d81997be6d3faa6a9302bff0eceb7
Christiaan Goossens <contact@christiaangoossens.nl>
2025-10-04 20:57:00 +0200

    Add test cases for the helpers

--- tests/test_hass_yaml_init.py
+++ tests/test_hass_yaml_init.py
@@ -7,30 +7,51 @@ from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
 from custom_components.auth_oidc import DOMAIN
+from custom_components.auth_oidc.config.const import ADDITIONAL_SCOPES
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def setup(hass: HomeAssistant, config: dict, expect_success: bool) -> bool:
+    """Set up the auth_oidc component."""
+    result = await async_setup_component(hass, DOMAIN, {DOMAIN: config})
+
+    if expect_success:
+        assert result
+        assert DOMAIN in hass.data
 
 
 @pytest.mark.asyncio
 async def test_setup_success_yaml(hass: HomeAssistant):
     """Test successful setup of a YAML configuration."""
-    mock_config = {
-        DOMAIN: {
+    await setup(
+        hass,
+        {
             "client_id": "dummy",
             "discovery_url": "https://example.com/.well-known/openid-configuration",
-        }
-    }
-    result = await async_setup_component(hass, DOMAIN, mock_config)
-    assert result
-    assert DOMAIN in hass.data
+        },
+        True,
+    )
+
+
+@pytest.mark.asyncio
+async def test_setup_success_yaml_with_optional(hass: HomeAssistant):
+    """Test successful setup of a YAML configuration with optional parameters."""
+    await setup(
+        hass,
+        {
+            "client_id": "dummy",
+            "discovery_url": "https://example.com/.well-known/openid-configuration",
+            ADDITIONAL_SCOPES: "email phone",
+        },
+        True,
+    )
 
 
 @pytest.mark.asyncio
 async def test_setup_failure_empty_yaml(hass: HomeAssistant, caplog):
     """Test failure setup of an empty YAML configuration."""
-    mock_config = {DOMAIN: {}}
-    result = await async_setup_component(hass, DOMAIN, mock_config)
-    assert not result
+    await setup(hass, {}, False)
 
     assert "required key 'client_id' not provided" in caplog.text
     assert "required key 'discovery_url' not provided" in caplog.text
@@ -43,13 +64,11 @@ async def test_setup_failure_empty_yaml(hass: HomeAssistant, caplog):
 @pytest.mark.asyncio
 async def test_setup_failure_partial_empty_yaml_discovery(hass: HomeAssistant, caplog):
     """Test failure setup of an partial YAML configuration."""
-    mock_config = {
-        DOMAIN: {
-            "discovery_url": "https://example.com/.well-known/openid-configuration"
-        }
-    }
-    result = await async_setup_component(hass, DOMAIN, mock_config)
-    assert not result
+    await setup(
+        hass,
+        {"discovery_url": "https://example.com/.well-known/openid-configuration"},
+        False,
+    )
 
     assert "required key 'client_id' not provided" in caplog.text
     assert "required key 'discovery_url' not provided" not in caplog.text
@@ -62,9 +81,12 @@ async def test_setup_failure_partial_empty_yaml_discovery(hass: HomeAssistant, c
 @pytest.mark.asyncio
 async def test_setup_failure_partial_empty_yaml_client(hass: HomeAssistant, caplog):
     """Test failure setup of an partial YAML configuration."""
-    mock_config = {DOMAIN: {"client_id": "test"}}
-    result = await async_setup_component(hass, DOMAIN, mock_config)
-    assert not result
+
+    await setup(
+        hass,
+        {"client_id": "test"},
+        False,
+    )
 
     assert "required key 'client_id' not provided" not in caplog.text
     assert "required key 'discovery_url' not provided" in caplog.text
---
+++ tests/test_helpers.py
@@ -0,0 +1,85 @@
+"""Tests for the YAML config setup of OIDC"""
+
+from unittest.mock import patch
+import pytest
+
+from aiohttp.test_utils import make_mocked_request
+
+from custom_components.auth_oidc.tools.helpers import get_url, get_view
+from custom_components.auth_oidc.tools.validation import (
+    validate_client_id,
+    sanitize_client_secret,
+    validate_discovery_url,
+    validate_url,
+)
+
+
+@pytest.mark.asyncio
+async def test_get_url():
+    """Test the get_url helper."""
+
+    with pytest.raises(RuntimeError) as excinfo:
+        get_url("https://example.com", "/test")
+    assert str(excinfo.value) == "No current request in context"
+
+    # Mock homeassistant.components.http.current_request.get() to test the force HTTP flag
+    with patch("homeassistant.components.http.current_request") as mock_current_request:
+        fake_request = make_mocked_request("GET", "http://example.com")
+        mock_current_request.get.return_value = fake_request
+        result = get_url("/test", True)
+        assert result == "https://example.com/test"
+
+
+@pytest.mark.asyncio
+async def test_get_view():
+    """Test the get_view helper."""
+
+    data = await get_view("welcome")
+    assert data.startswith("<!DOCTYPE html>")
+
+
+@pytest.mark.asyncio
+async def test_validate_url():
+    """Test the validate_url helper."""
+
+    assert not validate_url("ftp://example.com")
+    assert validate_url("http://example.com")
+    assert validate_url("https://example.com")
+    assert not validate_url("example.com")
+    assert not validate_url(42)
+    assert not validate_url([])
+
+
+@pytest.mark.asyncio
+async def test_validate_discovery_url():
+    """Test the validate_discovery_url helper."""
+
+    assert not validate_discovery_url("ftp://example.com")
+    assert not validate_discovery_url("http://example.com")
+    assert not validate_discovery_url("https://example.com")
+    assert not validate_discovery_url("example.com")
+    assert not validate_discovery_url(
+        "https://example.com/.well-known/openid_configuration"
+    )
+    assert validate_discovery_url(
+        "https://example.com/.well-known/openid-configuration"
+    )
+    assert not validate_discovery_url(2)
+    assert not validate_discovery_url([])
+
+
+@pytest.mark.asyncio
+async def test_client_secret():
+    """Test the sanitize_client_secret helper."""
+
+    assert sanitize_client_secret("test ") == "test"
+    assert sanitize_client_secret("test2") == "test2"
+
+
+@pytest.mark.asyncio
+async def test_client_id():
+    """Test the validate_client_id helper."""
+
+    assert not validate_client_id(" ")
+    assert validate_client_id("test4")
+    assert validate_client_id("test4 ")
